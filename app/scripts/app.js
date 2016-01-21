79c4ddad10f20d83bbecd92e458969110d05241d
Avinash Verma <eyedagger@gmail.com>
2016-01-21 22:19:09 +0530

    Init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name findingFalconeApp
+ * @description
+ * # findingFalconeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('findingFalconeApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name findingFalconeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the findingFalconeApp
+ */
+angular.module('findingFalconeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
